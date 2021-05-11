1328b4607de52aed7a60dc22ec2edec9d5eb4fec
Vladimir Drizheruk <vdrizheruk@oroinc.com>
2021-05-11 20:02:53 +0300

    php config

--- nix-config/generic/pkgs.nix
+++ nix-config/generic/pkgs.nix
@@ -3,9 +3,9 @@ let
   unstable = import <nixos-unstable> {};
   legacy = import <nixos-legacy> {};
 
-  php72 = pkgs.legacy.php72.buildEnv { extensions = { all, ... }: with all; [ imagick opcache ]; extraConfig = "memory_limit = 2048M"; };
-  php73 = pkgs.php73.buildEnv { extensions = { all, ... }: with all; [ imagick opcache ]; extraConfig = "memory_limit = 2048M"; };
-  php74 = pkgs.php74.buildEnv { extensions = { all, ... }: with all; [ imagick opcache ]; extraConfig = "memory_limit = 2048M"; };
+#  php72 = pkgs.legacy.php72.buildEnv { extensions = { all, ... }: with all; [ imagick opcache ]; extraConfig = "memory_limit = 2048M"; };
+#  php73 = pkgs.php73.buildEnv { extensions = { all, ... }: with all; [ imagick opcache ]; extraConfig = "memory_limit = 2048M"; };
+#  php74 = pkgs.php74.buildEnv { extensions = { all, ... }: with all; [ imagick opcache ]; extraConfig = "memory_limit = 2048M"; };
 #  unstable.php80 = pkgs.unstable.php80.buildEnv { extraConfig = "memory_limit = 4G"; };
 in
 {
@@ -52,15 +52,5 @@ in
     ansible_2_10 ansible-lint
     nodejs-14_x fpm mysql
     legacy.php72 php73 php74 unstable.php80
-    php74Extensions.gd php74Extensions.zip php74Extensions.xsl php74Extensions.xml php74Extensions.pdo
-    php74Extensions.gmp php74Extensions.ftp php74Extensions.ffi php74Extensions.dom
-    php74Extensions.dba php74Extensions.bz2 php74Extensions.ast php74Extensions.zlib php74Extensions.yaml
-    php74Extensions.tidy php74Extensions.soap php74Extensions.snmp php74Extensions.ldap
-    php74Extensions.json php74Extensions.intl php74Extensions.imap php74Extensions.exif php74Extensions.curl
-    php74Extensions.bz2 php74Extensions.shmop php74Extensions.redis php74Extensions.posix php74Extensions.pgsql
-    php74Extensions.pcntl php74Extensions.iconv php74Extensions.event php74Extensions.ctype php74Extensions.xmlrpc
-    php74Extensions.xdebug php74Extensions.mysqli php74Extensions.filter php74Extensions.bcmath
-
-
   ];
 }
